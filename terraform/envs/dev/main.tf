edd4aa3ca64a9f449e6feef05a9aeeb0a78f3a20
thaodinh97 <23521465@gm.uit.edu.vn>
2026-04-02 00:26:26 +0700

    test pipeline

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -4,7 +4,7 @@ module "vpc" {
   name            = "dev-vpc"
   cidr_block      = "10.0.0.0/16"
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_subnets = ["10.0.3.0/24", "10.0.5.0/24"]
+  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
 module "iam" {
